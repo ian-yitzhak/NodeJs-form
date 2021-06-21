4d48a66b6220d29b9ccfe9316a37d11296034ac2
ian-yitzhak <onyangoian71@gmail.com>
2021-06-21 14:31:56 +0300

    Add form logics

--- server.js
+++ server.js
@@ -3,5 +3,35 @@ require('./db.js')
 
 const app = express()
 
+
+app.set("view engine" , "ejs")
+app.use(express.urlencoded({ extended: false }))
+app.get('/', (req,res)=>{
+	res.render('home')
+})
+
+app.get('/success', (req,res)=>{
+	res.render('success')
+})
+
+app.post("/", async function (req, res) {
+
+	const context = {
+		name: req.body.name, 
+		email: req.body.email, 
+		contact: req.body.contact
+	}
+	try{
+		await context.save()
+		res.redirect('/success')
+	}catch(err){
+		console.log(err)
+	}
+  
+  
+
+ 
+});
+
 const port = 3000;
 app.listen(port, ()=> console.log(`listening on ${port}`))
